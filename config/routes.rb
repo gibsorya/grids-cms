55680bea0ed295cc505a0171eb05f79ffe193048
Ryan Gibson <gibsorya@gmail.com>
2022-05-01 19:41:05 -0500

    added login route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/login' => 'devise/sessions#create'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
