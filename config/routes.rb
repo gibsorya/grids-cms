cf029b6a341608589e309be016f2083281c688de
Ryan Gibson <gibsorya@gmail.com>
2022-05-05 09:01:10 -0500

    fixed logout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout"}
   devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/logout' => 'devise/sessions#destroy'
     get '/login' => 'devise/sessions#create'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
