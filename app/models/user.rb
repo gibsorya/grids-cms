09c72970c7c416791f086397e0981063b8598456
Ryan Gibson <gibsorya@gmail.com>
2022-05-06 16:59:29 -0500

    Updated devise to use username instead of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,14 @@ class User < ApplicationRecord
   enum role: { :user => 0, :moderator => 1, :admin => 2 }
   after_initialize :set_default_role, :if => :new_record?
 
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+  
   def set_default_role
     self.role ||= :user
   end
